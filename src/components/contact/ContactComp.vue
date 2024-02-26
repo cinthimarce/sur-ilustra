57fcdc3fed42db68ae926c966cc86b121aee520f
dacronik <dacronik@gmail.com>
2024-02-26 17:53:20 -0300

    agregando archivo env para proteger variables de desarrollo

--- src/components/contact/ContactComp.vue
+++ src/components/contact/ContactComp.vue
@@ -4,12 +4,12 @@
             <v-card-title primary-title class="text-center" mb-3>
                 Contactanos
             </v-card-title>
-            <v-card-media contain height="auto">
+            <v-card contain height="auto">
                 <iframe
                     src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2792.4700149870378!2d-72.06317142401875!3d-45.58109541652009!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xbdf2c49320a60dd7%3A0xc12e48cef5516c3b!2sLos%20Coig%C3%BCes%201237%2C%20Coyhaique%2C%20Ays%C3%A9n!5e0!3m2!1ses!2scl!4v1708561423694!5m2!1ses!2scl"
                     width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"
                     referrerpolicy="no-referrer-when-downgrade"></iframe>
-            </v-card-media>
+            </v-card>
             <v-row>
 
             </v-row>
