76468fe88758e8260b2fbe52f215447bac6c4019
dacronik <dacronik@gmail.com>
2024-02-26 18:18:16 -0300

    modificando archivos env

--- src/firebase/firebaseConfigExample.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-    apiKey: "AIzaSyDUFgdNO5kT7knu46azVejCZ1iHx_KxKCA",
-    authDomain: "galeria-sur-ilustra.firebaseapp.com",
-    projectId: "galeria-sur-ilustra",
-    storageBucket: "galeria-sur-ilustra.appspot.com",
-    messagingSenderId: "994235753051",
-    appId: "1:994235753051:web:6c5460291f4a57ff1cd57e",
-    measurementId: "G-C7DF0RTM6C"
-};
