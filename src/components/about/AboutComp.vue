255b5c4c3b59a74992f3279131f2dcc9c174e3c5
cinthimarce <106496881+cinthimarce@users.noreply.github.com>
2024-03-16 20:40:06 -0300

    Cambios en CarruselHome, ContactComp y AboutComp

--- src/components/about/AboutComp.vue
+++ src/components/about/AboutComp.vue
@@ -1,16 +1,16 @@
 <template>
-    <v-card class="mx-auto font" max-width="434" rounded="10" elevation="0" >
+    <v-card class="mx-auto font" max-width="500" rounded="10" elevation="0" >
         <v-img height="100%" cover src="@/assets/banner/canvas2.png">
             <v-avatar color="grey" size="150" rounded="0">
                 <v-img cover src="@/assets/banner/logohv.jpeg"></v-img>
             </v-avatar>
-            <v-list-item class="text-white " title="Humberto Vera" subtitle="Creador de Enmarcaciones Aysén"></v-list-item>
+            <v-list-item class="text-white" title="Humberto Vera" subtitle="Artista Visual"></v-list-item>
         </v-img>
     </v-card>
 
-    <v-card class="mx-auto" max-width="434" >
-    <v-card-text class="font-weight-regular font pb-0 pt-6 ">Desde que era niño, en ese imaginario personal, siempre existió y se mantuvo hasta el día de hoy, la idea de la relación existente entre hombre y naturaleza.  </v-card-text>
-    <v-card-text class="font-weight-regular font pb-6"> Ese olor a leña, a tejuela recién hecha, mezclada con la vegetación húmeda del bosque.  Las imágenes de éstas casitas, responden a cómo éstos materiales naturales una vez vivos , se transformaron en parte del mundo cultural, y como, poco a poco se van transformando nuevamente en naturaleza por el paso de los años. </v-card-text>
+    <v-card class="mx-auto" max-width="500" >
+    <v-card-text class="font-weight-regular text-center font pb-0 pt-6 ">Desde que era niño, en ese imaginario personal, siempre existió y se mantuvo fiel hasta el día de hoy, la idea de la relación existente entre hombre y naturaleza.  </v-card-text>
+    <v-card-text class="font-weight-regular text-center font pb-6"> Ese olor a leña, a tejuela recién hecha, mezclada con la vegetación húmeda del bosque.  Las imágenes de éstas casitas, responden a cómo éstos materiales naturales una vez vivos , se transformaron en parte del mundo cultural, y como, poco a poco se van transformando nuevamente en naturaleza por el paso de los años. </v-card-text>
     </v-card>
 </template>
 
--- src/components/home/CarouselHome/CarouselHome.vue
+++ src/components/home/CarouselHome/CarouselHome.vue
@@ -1,20 +1,40 @@
 <template>
-    <v-carousel hide-delimiters>
+    <v-carousel hide-delimiters cycle >
       <v-carousel-item src="@/assets/banner/casa1.webp" @click="redirectTo()"></v-carousel-item>
-      <v-carousel-item src="@/assets/banner/casa2.webp" @click="redirectTo()"></v-carousel-item>
-      <v-carousel-item src="@/assets/banner/casa3.webp" @click="redirectTo()"></v-carousel-item>
+      <v-carousel-item class="d-flex justify-content-center align-items-center " >
+        <v-card class="d-flex align-items-center fill-height pt-4 mx-auto item" elevation="0">
+          <v-card-text class="d-flex justify-center align-center">
+            <v-card-text class="text-h6 text-center font-carrusel ">
+              Bienvenido al taller Enmarca  Aysén, conoce mi obras inspiradas en edificaciones antiguas y animales representativos de la región de Aysén.
+            </v-card-text>
+          </v-card-text>
+        </v-card>
+      </v-carousel-item>
       <v-carousel-item src="@/assets/banner/avecilla1.webp" @click="redirectTo()"></v-carousel-item>
+      <v-carousel-item>
+        <v-card class="d-flex align-items-center fill-height pt-4 mx-auto item" elevation="0">
+          <v-card-text class="d-flex justify-center align-center">
+            <v-card-text class="text-h6 text-center font-carrusel ">
+              Las obras se realizan a mano alzada en base a acuarela y tinta, los papeles aseguran la calidad y durabilidad de cada una de las piezas .
+            </v-card-text>
+          </v-card-text>
+        </v-card>
+      </v-carousel-item>
+      <v-carousel-item src="@/assets/banner/casa2.webp" @click="redirectTo()"></v-carousel-item>
+      <v-carousel-item>
+        <v-card class="d-flex align-items-center fill-height pt-4 mx-auto item" elevation="0">
+          <v-card-text class="d-flex justify-center align-center">
+            <v-card-text class="text-h6 text-center font-carrusel ">
+              Cada acuarela se encuentra firmada con un cuño seco, correspondiente a un anagrama de las iniciales de mi nombre, Humberto Adolfo Vera Núñez.
+            </v-card-text>
+          </v-card-text>
+        </v-card>
+      </v-carousel-item>
       <v-carousel-item src="@/assets/banner/avecilla2.webp" @click="redirectTo()"></v-carousel-item>
+      <v-carousel-item src="@/assets/banner/casa3.webp" @click="redirectTo()"></v-carousel-item>
     </v-carousel>
-    <v-card class="mx-auto" width="850px" elevation="0">
-    <v-card-text class="d-flex flex-column fill-height justify-center align-center text-black">
-      <p class="text-h6 font-weight-thin mb-4 text-center font-carrusel">
-      Bienvenido al taller Enmarca  Aysén, conoce mi obras inspiradas en edificaciones antiguas y animales representativos de la región de Aysén.
-      </p>
-    </v-card-text>
-  </v-card>
   </template>
-    
+  
   <script setup>
   import { defineEmits } from 'vue';
 
@@ -26,4 +46,31 @@ const redirectTo = (title,id) => {
 };
   </script>
     
-  <style scoped></style>
+  <style scoped>
+  .font-carrusel{
+    font-family: 'Homemade Apple', cursive !important;
+    color: #315467;
+    line-height: 1.5;
+
+  }
+  .min{
+  display: flex;
+  flex-wrap: wrap;
+  }
+  .item {
+  width: 100%;
+  }
+
+@media (min-width: 150px) {
+  .item {
+  width: 80%;
+  }
+}
+
+@media (max-width: 1024px) {
+  .item {
+  width: 50%;
+  }
+}
+  
+  </style>
